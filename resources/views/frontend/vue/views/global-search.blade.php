904408c547f24b3212933d3ec9a4ce547eb81cfd
Arham1243 <arham404khan@gmail.com>
2025-12-10 19:49:36 +0500

    Added Holidays, UAE Visa Change, and Offers navigation links to global search pill tabs

--- resources/views/frontend/vue/views/global-search.blade.php
+++ resources/views/frontend/vue/views/global-search.blade.php
@@ -9,6 +9,9 @@
                                 data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home"
                                 aria-selected="true">Flight</button>
                         </li>
+                        <li>
+                            <a href="#" class="search-pill">Holidays</a>
+                        </li>
                         <li role="presentation">
                             <button class="search-pill" id="pills-profile-tab" data-bs-toggle="pill"
                                 data-bs-target="#pills-profile" type="button" role="tab"
@@ -21,6 +24,12 @@
                         </li>
                         <li>
                             <a href="#" class="search-pill">UAE Visa</a>
+                        </li>
+                        <li>
+                            <a href="#" class="search-pill">UAE Visa Change</a>
+                        </li>
+                        <li>
+                            <a href="#" class="search-pill">Offers</a>
                         </li>
                     </ul>
                 </div>
