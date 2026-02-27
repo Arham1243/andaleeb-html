9c229723408b677fb32a2568f9ffbb592971aca7
Arham1243 <arham404khan@gmail.com>
2026-02-27 16:56:50 +0500

    add debug logging to inspect Dubai province and its locations in hotel search

--- resources/views/frontend/vue/services/hotels-search.blade.php
+++ resources/views/frontend/vue/services/hotels-search.blade.php
@@ -9,6 +9,16 @@
         locations
     }));
 
+    const {
+        provinces,
+        locations
+    } = await hotelsDataPromise;
+    const dubai = provinces.find(p => p.name.toLowerCase() === 'dubai');
+    console.log('Dubai province:', dubai);
+    console.log('Dubai province_id:', dubai?.id);
+    console.log('Locations with that province_id:', locations.filter(l => l.province_id === dubai?.id));
+    console.log('Sample location:', locations.find(l => l.name === 'Dubai Marina'));
+
     const formatResults = ({
         countries,
         provinces,
