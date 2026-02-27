f49663a82c927af927a5f0424fface8af5ec6025
Arham1243 <arham404khan@gmail.com>
2026-02-27 16:36:08 +0500

    revert asset paths from user back to frontend in hotel search mock data fetches

--- resources/views/frontend/vue/services/hotels-search.blade.php
+++ resources/views/frontend/vue/services/hotels-search.blade.php
@@ -1,8 +1,8 @@
 <script>
     const hotelsDataPromise = Promise.all([
-        fetch("{{ asset('user/mocks/yalago_countries.json') }}").then(r => r.json()),
-        fetch("{{ asset('user/mocks/yalago_provinces.json') }}").then(r => r.json()),
-        fetch("{{ asset('user/mocks/yalago_locations.json') }}").then(r => r.json())
+        fetch("{{ asset('frontend/mocks/yalago_countries.json') }}").then(r => r.json()),
+        fetch("{{ asset('frontend/mocks/yalago_provinces.json') }}").then(r => r.json()),
+        fetch("{{ asset('frontend/mocks/yalago_locations.json') }}").then(r => r.json())
     ]).then(([countries, provinces, locations]) => ({
         countries,
         provinces,
